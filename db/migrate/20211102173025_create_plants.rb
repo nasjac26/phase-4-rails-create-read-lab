a24556578815c73beebef3611028d43cd0c3b034
Jacob <Jacobonasrallah@gmail.com>
2021-11-02 15:23:23 -0700

    commit tree 9448d2f04f9102bef0c453f4e3014b92a7761cfb

---
+++ db/migrate/20211102173025_create_plants.rb
@@ -0,0 +1,12 @@
+class CreatePlants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :plants do |t|
+      t.string :name
+      t.string :n
+      t.string :image
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
